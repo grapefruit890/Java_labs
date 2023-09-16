ec438ca323e42e0475945bacfd0630cb3a94da94
grapefruit890 <rybstep@yandex.ru>
2023-09-16 11:38:59 +0300

    changes

---
+++ Lab4/src/laba/Main.java
@@ -0,0 +1,66 @@
+package laba;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        while (true) {
+            System.out.println("\nENTER THE TASK NUMBER");
+
+            Scanner in = new Scanner(System.in);
+            int task = in.nextInt();
+
+            switch (task) {
+                case 0:
+                    System.out.println("EXIT");
+                    return;
+                case 1:
+                    ex1();
+                    break;
+                case 2:
+//                    ex2(args);
+                    break;
+            }
+
+        }
+    }
+
+
+    public enum Seasons {
+        WINTER {
+
+        },
+        SPRING,
+        SUMMER,
+        AUTUMN,
+        TEMP_SPRING,
+        TEMP_SUMMER;
+
+        fun getTemp()
+
+    }
+
+    public static void ilove(Seasons season) {
+        switch (season) {
+            case WINTER:
+                System.out.println("I love winter");
+                break;
+            case SPRING:
+                System.out.println("I love spring");
+                break;
+            case SUMMER:
+                System.out.println("I love summer");
+                break;
+            case AUTUMN:
+                System.out.println("I love autumn");
+                break;
+            default:
+                break;
+        }
+    }
+
+    public static void ex1() {
+        System.out.println(Seasons.TEMP_AUTUMN);
+    }
+
+}
